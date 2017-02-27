9e1006a7f94bd610f5a8a7781b41baa3921a4568
arjaynguyen <arjaynguyen123@hotmail.com>
2017-02-27 14:30:50 -0800

    user form able to upload images

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.ext import ndb
+from google.appengine.api import images
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
@@ -19,11 +22,36 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Post(ndb.Model):
+	title = ndb.StringProperty(required=True)
+	content = ndb.TextProperty(required=True)
+	header_image = ndb.BlobProperty()
+	created = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainPage(Handler):
     def get(self):
     	self.render("blog.html")
 
+class NewPostPage(Handler):
+	def get(self):
+		self.render("newpost.html")
 
-app = webapp2.WSGIApplication([('/', MainPage)])
+	def post(self):
+		title = self.request.get("title")
+		content = self.request.get("content")
+
+		header_image = self.request.get("img")
+		header_image = images.resize(header_image, 500, 200)
+
+		valid = True
+
+		post = Post(title=title, content=content)
+		post.header_image = header_image
+
+		post.put()
+
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+							   ('/newpost', NewPostPage),])
