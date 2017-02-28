cf480d73c960ccffaf9e4e30bd835767958698c5
arjaynguyen <arjaynguyen123@hotmail.com>
2017-02-28 14:07:54 -0800

    implemented signup partially, still need to create cookies

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 import jinja2
 import webapp2
+import re
+import hmac
+import random
+import hashlib
 
+from models.user import User
+from string import letters
 from google.appengine.ext import db
 from google.appengine.api import images
 
@@ -9,6 +15,10 @@ from google.appengine.api import images
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
                                autoescape=True)
+
+secret = "imsosecret"
+
+
 
 
 class Handler(webapp2.RequestHandler):
@@ -22,59 +32,167 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    def make_secure_val(val):
+        return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+    def check_secure_val(secure_val):
+        val = secure_val.split('|')[0]
+        if secure_val == make_secure_val(val):
+            return val
+
+
+class UserSettingsHandler(Handler):
+    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    def valid_username(self, username):
+        return username and self.USER_RE.match(username)
+
+    PASS_RE = re.compile(r"^.{3,20}$")
+    def valid_password(self, password):
+        return password and self.PASS_RE.match(password)
+
+    EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+    def valid_email(self, email):
+        return not email or self.EMAIL_RE.match(email)
+
+
 
 
 class Post(db.Model):
-	title = db.StringProperty(required=True)
-	content = db.TextProperty(required=True)
-	header_image = db.BlobProperty()
-	created = db.DateTimeProperty(auto_now_add=True)
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    header_image = db.BlobProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+
+
+class UserPage(Handler):
+    def get(self):
+        self.render("user.html")
+
+class SignupPage(UserSettingsHandler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.request.get("username")
+        first_name = self.request.get("first_name")
+        last_name = self.request.get("last_name")
+        password = self.request.get("password")
+        confirm_pw = self.request.get("confirm_pw")
+        email = self.request.get("email")
+
+        valid = True
+        username_error = ""
+        first_name_error = ""
+        last_name_error = ""
+        password_error = ""
+        confirm_pw_error = ""
+        email_error = ""
+
+        retry_params = { "username": username,
+                         "first_name": first_name,
+                         "last_name": last_name,
+                         "email": email }
+
+        if not self.valid_username(username):
+            valid = False
+            retry_params["username_error"] = "Username is invalid"
+
+        if not first_name:
+            valid = False
+            retry_params["first_name_error"] = "Please enter a first name"
+
+        if not last_name:
+            valid = False
+            retry_params["last_name_error"] = "Please enter a last name"
+
+        if not self.valid_password(password):
+            valid = False
+            retry_params["password_error"] = "Password is invalid"
+        elif password != confirm_pw:
+            valid = False
+            retry_params["confirm_pw_error"] = "Passwords do not match"
+
+        if not self.valid_email(email):
+            valid = False
+            retry_params["email_error"] = "Email is invalid"
+        
+
+        if not valid:
+            self.render("signup.html", **retry_params)
+
+        else:
+
+            if User.username_in_use(username):
+                retry_params["username_error"] = "Username in use"
+                self.render("signup.html", **retry_params)
+                print(User.username_in_use(username))
+                return
+
+
+            user = User.register(username, 
+                                 first_name, 
+                                 last_name, 
+                                 password, 
+                                 email)
+
+            avatar_image = self.request.get("img")
+            if avatar_image:
+                avatar_image = images.resize(avatar_image, 150, 150)
+                user.avatar_image = avatar_image
+
+            
+            user.put()
+            self.redirect("/")
+
+
 
 
 class ImageHandler(Handler):
-	def get(self):
-		img_id = self.request.get("img_id")
-		if img_id and img_id.isdigit():
+    def get(self):
+        img_id = self.request.get("img_id")
+        if img_id and img_id.isdigit():
 
-			post = Post.get_by_id(int(img_id))
-			
+            post = Post.get_by_id(int(img_id))
+            
 
-			if post and post.header_image:
-				self.response.headers["Content-Type"] = "image/jpeg"
-				self.response.out.write(post.header_image)
-			else:
-				print "poopoo"
+            if post and post.header_image:
+                self.response.headers["Content-Type"] = "image/jpeg"
+                self.response.out.write(post.header_image)
+            else:
+                print "poopoo"
 
-		else:
-			print "poop"
-
-
+        else:
+            print "poop"
 
 class MainPage(Handler):
     def get(self):
-    	posts_cursor = Post.gql("ORDER BY created DESC")
-    	posts = posts_cursor.fetch(limit=10)
-    	self.render("blog.html", posts=posts)
+        posts_cursor = Post.gql("ORDER BY created DESC")
+        posts = posts_cursor.fetch(limit=10)
+        self.render("blog.html", posts=posts)
 
 class NewPostPage(Handler):
-	def get(self):
-		self.render("newpost.html")
+    def get(self):
+        self.render("newpost.html")
 
-	def post(self):
-		title = self.request.get("title")
-		content = self.request.get("content")
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
 
-		header_image = self.request.get("img")
-		header_image = images.resize(header_image, 500, 200)
+        header_image = self.request.get("img")
+        header_image = images.resize(header_image, 500, 200)
 
-		valid = True
+        valid = True
 
-		post = Post(title=title, content=content)
-		post.header_image = header_image
-		post.put()
+        post = Post(title=title, content=content)
+        post.header_image = header_image
+        post.put()
 
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-							   ('/newpost', NewPostPage),
-							   ('/img', ImageHandler)])
+                               ('/newpost', NewPostPage),
+                               ('/img', ImageHandler),
+                               ('/signup', SignupPage),
+                               ('/user', UserPage)])
---
+++ models/user.py
@@ -0,0 +1,43 @@
+import random
+import hashlib
+
+from string import letters
+from google.appengine.ext import db
+
+
+##### user stuff
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+class User(db.Model):
+    username = db.StringProperty(required=True)
+    first_name = db.StringProperty(required=True)
+    last_name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+    avatar_image = db.BlobProperty()
+
+
+    @classmethod 
+    def username_in_use(cls, username):
+        return User.gql("WHERE username = '%s'" % username).get()
+
+
+    @classmethod
+    def register(cls, username, first_name, last_name, pw,
+                 email = None, avatar_image=None):
+
+        pw_hash = make_pw_hash(username, pw)
+        return User(username = username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    pw_hash = pw_hash,
+                    email = email,
+                    avatar_image=avatar_image)
