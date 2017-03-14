3f59f832df050ceb28987104e247c0fa031bc7b8
arjaynguyen <arjaynguyen123@hotmail.com>
2017-03-14 15:30:42 -0700

    commenting on comment, heh

--- models/comment.py
+++ models/comment.py
@@ -2,19 +2,41 @@ from google.appengine.ext import db
 from user import User
 
 class Comment(db.Model):
-	post_id = db.IntegerProperty(required=True)
-	user_id = db.IntegerProperty(required=True)
-	content = db.TextProperty(required=True)
-	created = db.DateTimeProperty(auto_now_add=True)
+    """ A comment submitted by a user about a Post entity
 
-	def get_user_name(self):
-		if self.user_id:
-			key = db.Key.from_path('User', int(self.user_id))
-			user = db.get(key)
+    Attributes:
+        post_id: id of the post this comment is tied to
+        user_id: id of the user who submitted this comment
+        content: body of comment
+        created: when comment was posted
+    """
+    post_id = db.IntegerProperty(required=True)
+    user_id = db.IntegerProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
-			if user:
-				return user.get_full_name()
+    def get_user_name(self):
+        """ Gets first and last name of user tied to this comment
 
-	def get_formatted_text(self):
-		if self.content:
-			return self.content.replace('\n', '<br>')
+        Returns:
+            first and last name of user as a string
+        """
+        if self.user_id:
+            key = db.Key.from_path('User', self.user_id)
+            user = db.get(key)
+
+            if user:
+                return user.get_full_name()
+
+
+    def get_formatted_text(self):
+        """ Gets content formatted for html pages
+
+        Returns:
+            Formatted content
+        """
+        if self.content:
+            return self.content.replace('\n', '<br>')
+
+
+
